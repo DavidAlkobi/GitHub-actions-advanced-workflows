c4a1ec4f70dd0541714993194f80fcb08d48e984
DavidAlkobi <davidalk3@gmail.com>
2024-01-05 13:46:56 +0200

    add deplot to s3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,19 @@
 const core=require('@actions/core')
-const github=require('@actions/github')
+// const github=require('@actions/github')
 const exec=require('@actions/exec')
 
 function run(){
+    // Get some input values
+    const bucket = core.getInput('bucket' , {require: true})
+    const region = core.getInput('region' , {require: true})
+    const distFolder = core.getInput('dist-folder' , {require: true})
+
+    // Upload files to s3 using aws cli command
+    const s3uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3uri} --region ${region}`) ;
+
+
+
     core.notice('Hello from my JS action')
 
 }
